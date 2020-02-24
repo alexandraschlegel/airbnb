4dd9f00c6496ea3cd42c42af28ca7f4610ebc4a9
Alexandra Schlegel <alexandra.schlegel@yahoo.de>
2020-02-24 17:08:55 +0000

    validation added

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,8 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :flat
+
+  validates :date_from, presence: true
+  validates :date_to, presence: true
+
 end
--- app/models/flat.rb
+++ app/models/flat.rb
@@ -1,3 +1,7 @@
 class Flat < ApplicationRecord
   belongs_to :user
+  has_many :bookings
+  has_many :users, through: :bookings
+
+  validates :address, :price, :capacity, presence: true
 end
