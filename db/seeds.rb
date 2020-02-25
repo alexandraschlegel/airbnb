5ff462895e925592422ea415bd9689402ba37c28
Priyesh patel <priyeshpatel9843516@icloud.com>
2020-02-25 12:35:02 +0000

    new seed updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,17 +5,17 @@ Flat.destroy_all
 User.destroy_all
 
 puts "Creating users"
-user1 = User.create!(email: "test1@gmail.com", password: "123456", password_confirmation: "123456")
-user2 = User.create!(email: "test2@gmail.com", password: "123456", password_confirmation: "123456")
-user3 = User.create!(email: "test3@gmail.com", password: "123456", password_confirmation: "123456")
+elizabeth = User.create!(email: "elizabeth@gmail.com", password: "htebazile", password_confirmation: "htebazile")
+henry = User.create!(email: "henry's@gmail.com", password: "pasword", password_confirmation: "pasword")
+charles = User.create!(email: "emperorpalpatine@gmail.com", password: "darkside", password_confirmation: "darkside")
 puts "Creating flats"
-flat1 = Flat.create!(address:'London,Bodiam,Robertsbridge TN32 5UA',rating:2,price:400,capacity:450,facilities:'has a shower, has a bathroom,nothing too spectacular!', user:user1)
-flat2 = Flat.create!(address:'Paris venue de Paris, 94300 Vincennes,France',rating:6,price:700,capacity:800,facilities:'a underground basement to  do some sketchy stuff', user:user2)
-flat3 = Flat.create!(address:'Austria 13, Moosham, 5585 Unternberg,',rating:8,price:1200,capacity:1000,facilities:'cells to hold prisoners,large swimming pool,large kitchen', user:user3)
+flat1 = Flat.create!(address:'London,Bodiam,Robertsbridge TN32 5UA',rating:2,price:400,capacity:450,facilities:'has a shower, has a bathroom,nothing too spectacular!', user:elizabeth)
+flat2 = Flat.create!(address:'Paris venue de Paris, 94300 Vincennes,France',rating:6,price:700,capacity:800,facilities:'a underground basement to  do some sketchy stuff', user:henry)
+flat3 = Flat.create!(address:'Austria 13, Moosham, 5585 Unternberg,',rating:8,price:1200,capacity:1000,facilities:'cells to hold prisoners,large swimming pool,large kitchen', user:charles)
 puts "Creating bookings"
-booking1 = Booking.create!(date_from: Time.now, date_to: Time.now + 3000 , user:user1, flat:flat1)
-booking2 = Booking.create!(date_from: Time.now, date_to: Time.now + 3000 , user:user2, flat:flat2)
-booking3 = Booking.create!(date_from: Time.now, date_to: Time.now + 3000 , user:user3, flat:flat3)
+booking1 = Booking.create!(date_from: Time.now, date_to: Time.now + 3000 , user:Elizabeth, flat:flat1)
+booking2 = Booking.create!(date_from: Time.now, date_to: Time.now + 3000 , user:Henry, flat:flat2)
+booking3 = Booking.create!(date_from: Time.now, date_to: Time.now + 3000 , user:Charles, flat:flat3)
 
 
 
