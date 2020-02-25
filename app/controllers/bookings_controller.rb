aeb2eba8409bbaaf172c4099c7e6cf790ead8e5b
Naomi Arefaine <naomiarefaine@gmail.com>
2020-02-25 12:00:25 +0000

    multiple methods added- see github comments

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -17,12 +17,28 @@ class BookingsController < ApplicationController
   end
 
   def show
+    @booking = Booking.find(params[:booking_id])
   end
+
+  def edit
+    @booking = Booking.find(params[:booking_id])
+  end
+
+  def update
+   @booking = Booking.find(params[:booking_id])
+    @booking = Booking.update(bookings_params)
+    @booking.flat = @flat
+    if @booking.save
+      redirect_to flat_path(@flat)
+    else
+      render :edit
+  end
+end
 
 private
 
   def bookings_params
-    params.require(:booking).permit(:address, :capacity, :date_to, :date_from, :flat_id) #:dose_id
+    params.require(:booking).permit(:address, :capacity, :date_to, :date_from)
   end
 end
 
