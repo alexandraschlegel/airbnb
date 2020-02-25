bf8401fc13cf2724fc7f37681ddb48327d020c68
Naomi Arefaine <naomiarefaine@gmail.com>
2020-02-25 10:38:34 +0000

    only created new and create method for bookings, not flats. Not finished home page

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,13 +1,28 @@
 class BookingsController < ApplicationController
 
   def new
+    @flat = Flat.find(params[:flat_id])
+    @booking = Booking.new
   end
 
   def create
+    @flat = Flat.find(params[:flat_id])
+    @booking = Booking.new(bookings_params)
+    @booking.flat = @flat
+    if @booking.save
+      redirect_to flat_path(@flat)
+    else
+      render :new
+    end
   end
 
   def show
   end
 
+private
 
+  def bookings_params
+    params.require(:booking).permit(:address, :capacity, :date_to, :date_from, :flat_id) #:dose_id
+  end
 end
+
