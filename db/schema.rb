fab99d4f5e89d21f8e09eab516e8e1d25703e20a
Naomi Arefaine <naomiarefaine@gmail.com>
2020-02-28 11:51:10 +0000

    new master

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_28_104244) do
+ActiveRecord::Schema.define(version: 2020_02_28_113145) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,6 +60,7 @@ ActiveRecord::Schema.define(version: 2020_02_28_104244) do
     t.integer "moats"
     t.integer "draw_bridges"
     t.string "name"
+    t.string "image"
     t.index ["user_id"], name: "index_flats_on_user_id"
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@ User.destroy_all
 
 
 # puts "Creating users"
-# elizabeth = User.create!(email: "elizabeth@hermajesty.com", password: 123456, photo:'Queeny.png')
+elizabeth = User.create!(email: "elizabeth@hermajesty.com", password: 123456)
 # nemo = User.create!(email: "nemo@whereami.com", password:1234567, photo: 'nemo.jpg')
 # sissi = User.create!(email: "sissi@wannabeaprincess.com",password: 12345678)
 # flamingo = User.create!(email:"flamingo@pinkitwasloveatfirstsight.com",password:123456789, photo:'flamingo.jpg')
@@ -17,14 +17,14 @@ User.destroy_all
 
 
 # puts "Creating flats"
-# flat1 = Flat.create!(name:'Taj Mahal',
-#   address: 'Dharmapuri, Forest Colony, Tajganj, Agra, Uttar Pradesh 282001, India',
-#   rating: 5,
-#   price: 400,
-#   capacity: 450,
-#   facilities:'Tea room, Outdoor swimming pool',
-#   user: elizabeth,
-#   photo: 'tajmahal.jpg')
+flat1 = Flat.create!(name:'Taj Mahal',
+  address: 'Dharmapuri, Forest Colony, Tajganj, Agra, Uttar Pradesh 282001, India',
+  rating: 5,
+  price: 400,
+  capacity: 450,
+  facilities:'Tea room, Outdoor swimming pool',
+  user: elizabeth,
+  image: Cloudinary::Uploader.upload("app/assets/images/BuckinghamPalace.jpg")["public_id"])
 
 # flat2 = Flat.create!(name:'Buckingham Palace',
 #   address: 'Westminster, London SW1A 1AA',
@@ -33,7 +33,7 @@ User.destroy_all
 #   capacity: 1000,
 #   facilities: 'Ball room, Tea salon, Butler Service',
 #   user: elizabeth,
-#   photo: 'BuckinghamPalace.jpg')
+#   image: 'BuckinghamPalace.jpg')
 
 # flat3 = Flat.create!(name:'The White House',
 #   address: '1600 Pennsylvania Ave NW, Washington, DC 20500, United States',
@@ -42,7 +42,7 @@ User.destroy_all
 #   capacity: 1200,
 #   facilities:'Conference room, Helipad, Panic Room',
 #   user: donald,
-#   photo: 'whitehouse.jpg')
+#   image: 'whitehouse.jpg')
 
 # flat4 = Flat.create!(name:'Schloss Neuschwanstein',
 #   address: 'Neuschwansteinstraße 20, 87645 Schwangau, Germany',
@@ -51,7 +51,7 @@ User.destroy_all
 #   capacity: 1000,
 #   facilities:'Ball Room, Tourture Chamber',
 #   user:sissi,
-#   photo: 'SchlossNeuschwanstein.jpg')
+#   image: 'SchlossNeuschwanstein.jpg')
 
 # flat5 = Flat.create!(name:'Great Barrier Reef',
 #   address: '24 The Cove Rd, Airlie Beach QLD 4802, Australia',
@@ -60,7 +60,7 @@ User.destroy_all
 #   capacity: 1000,
 #   facilities:'Free snorkel equipment included',
 #   user:nemo,
-#   photo: 'greatbarrierreef.jpg')
+#   image: 'greatbarrierreef.jpg')
 
 # flat6 = Flat.create!(name:'Pentagon',
 #   address: 'Washington, DC 22202, United States',
@@ -69,7 +69,7 @@ User.destroy_all
 #   capacity: 9000,
 #   facilities:'Panic room, Secret Tunnel, Professional security system',
 #   user:donald,
-#   photo: 'The_Pentagon_cropped_square.png')
+#   image: 'The_Pentagon_cropped_square.png')
 
 # flat7 = Flat.create!(name:'The great wall of China',
 #   address:'Huairou District, China',
@@ -78,7 +78,7 @@ User.destroy_all
 #   capacity: 1000,
 #   facilities:'Wonderful walking track',
 #   user:wladimir,
-#   photo: 'greatwallofchina.jpg')
+#   image: 'greatwallofchina.jpg')
 
 # flat8 = Flat.create!(name:'Moscow Kremlin',
 #   address: 'Moscow, Russia, 103073',
@@ -87,7 +87,7 @@ User.destroy_all
 #   capacity: 1000,
 #   facilities:'Secret Tunnel, Spiral Staircase, Magic Room',
 #   user:wladimir,
-#   photo: 'kremlin.jpg')
+#   image: 'kremlin.jpg')
 
 # flat9 = Flat.create!(name:'Great Pyramid of Giza',
 #   address: 'Al Haram, Nazlet El-Semman, Al Giza Desert, Giza Governorate, Egypt',
@@ -96,7 +96,7 @@ User.destroy_all
 #   capacity: 1000,
 #   facilities:'Mummies, Natural Air Conditioning',
 #   user:cleopatra,
-#   photo: 'pyramids.jpg')
+#   image: 'pyramids.jpg')
 
 # flat10 = Flat.create!(name:'Bolivian Sea Desert',
 #   address: 'Daniel Campos Province',
@@ -105,7 +105,7 @@ User.destroy_all
 #   capacity: 1000,
 #   facilities: 'Flamingos, Fresh and Saltwater Pool, 365 Days of Sun, Spacious Outdoor space',
 #   user:flamingo,
-#   photo: 'saltdesert.jpg')
+#   image: 'saltdesert.jpg')
 
 # flat11 = Flat.create!(name:'Maccu Piccu',
 #   address:'08680, Peru',
@@ -114,7 +114,7 @@ User.destroy_all
 #   capacity: 1000,
 #   facilities:'Wonderful walking track for the keen hiker, Spectular view, Back to Nature Experience',
 #   user:flamingo,
-#   photo: 'macchupiccu.jpg')
+#   image: 'macchupiccu.jpg')
 
 # flat12 = Flat.create!(name:'Eiffel Tower',
 #   address: 'Champ de Mars, 5 Avenue Anatole France, 75007 Paris, France',
@@ -123,7 +123,7 @@ User.destroy_all
 #   capacity: 1000,
 #   facilities:'Airy, Great City Views',
 #   user:monalisa,
-#   photo: 'eiffeltower.jpg')
+#   image: 'eiffeltower.jpg')
 
 # flat13 = Flat.create!(name:'Statue of Liberty',
 #   address: 'New York, NY 10004, United States',
@@ -132,7 +132,7 @@ User.destroy_all
 #   capacity: 1000,
 #   facilities:'Skyline views',
 #   user:donald,
-#   photo: 'StatueofLiberty.jpg')
+#   image: 'StatueofLiberty.jpg')
 
 # flat14 = Flat.create!(name:'Louvre',
 #   address: 'Rue de Rivoli, 75001 Paris, France',
@@ -141,7 +141,7 @@ User.destroy_all
 #   capacity: 60000,
 #   facilities:'Acclaimed International Art',
 #   user:monalisa,
-#   photo: 'louvre.jpg')
+#   image: 'louvre.jpg')
 
 # puts "Creating bookings"
 # booking1 = Booking.create!(date_from: Time.now, date_to: Time.now + 3000 , user:elizabeth, flat:flat1)
